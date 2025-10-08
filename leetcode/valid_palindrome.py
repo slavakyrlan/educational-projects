73c91401cb470088f9c783663ff3ad484aa21a91
Вячеслав <slavakyrlan00@mail.ru>
2025-10-08 23:04:46 +0300

    08.10

--- leetcode/valid_palindrome.py
+++ leetcode/valid_palindrome.py
@@ -1,23 +1,26 @@
 """
-Задача:
-Подсказки:
+Задача: https://leetcode.com/problems/valid-palindrome/
+Подсказки: https://pyhub.ru/python/lecture-5-10-1029/
 """
 
-
 class Solution(object):
-    def example(self, *args, **kwargs):
+    def isPalindrome(self, s: str) -> bool:
         """Решение задачи"""
+        letters_only = [char.lower() for char in s if char.isalnum()]
+        return letters_only == letters_only[::-1]
 
 
 if __name__ == '__main__':
     cls = Solution()
 
     tests = [
-        ('x', 'y'),
+        ("A man, a plan, a canal: Panama", True),
+        ("race a car", False),
+        (" ", True),
     ]
 
     for i, (x, expected) in enumerate(tests, 1):
-        result = cls.example(x)
+        result = cls.isPalindrome(x)
         status = "✓" if result == expected else "✗"
         print(f' {status} Тест {i}: strs="{x}" -> {result=} (ожидается {expected=})')
         # assert result == expected, f"Тест {i} не пройден"
