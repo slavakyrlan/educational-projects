5b6e9aab3b967bd102fbdf94172b4752de377101
Вячеслав <slavakyrlan00@mail.ru>
2025-10-09 16:10:28 +0300

    9.10

--- single_number.py
+++ single_number.py
@@ -1,12 +1,29 @@
 """
 Задача: https://leetcode.com/problems/single-number/
 Подсказки:
+- https://javarush.com/groups/posts/operator-xor-v-python
+- https://www.youtube.com/watch?v=myW_9B9T_II
 """
 
 
 class Solution(object):
     def singleNumber(self, nums: list[int]) -> int:
         """Решение задачи"""
+        numer = {}
+        for i in nums:
+            numer[i] = numer.get(i, 0) + 1
+        for num, count in numer.items():
+            if count == 1:
+                return num
+
+    def singleNumber2(self, nums: list[int]) -> int:
+        return 2 * sum(set(nums)) - sum(nums)
+
+    def singleNumber3(self, nums: list[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
 
 
 if __name__ == '__main__':
